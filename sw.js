513da89d3dc9ccba01e9351f906c89c7fbe1ac4d
viteklu15 <46672002+viteklu15@users.noreply.github.com>
2025-06-03 10:43:36 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'premium-calculator-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
